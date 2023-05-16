9c9ee595258c61fcc3121d7bf949f60c152b746d
Bob Haffner <bob.haffner@gmail.com>
2023-05-16 10:41:54 -0500

    stashing work

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # chess_positions
 
 ## Chess Flash Card POC
-![Image](images/flash_cards.jpg)
+![Image](images/9afad515-883c-44e3-804f-53bf60d72fab_14_front.jpg)
+![Image](images/9afad515-883c-44e3-804f-53bf60d72fab_14_back.jpg)
+
+
 
 ## ToDos
-### * investigate delaying board.push_move to clean up loop
-### * classify cards ( eg missed capture, missed check, etc)
-### * only iterate thru my moves
+### * Classify cards ( eg missed capture, missed check, etc)
+### * Read PGNs.  This would open it up to Lichess games and leverage chess com and lichess UI to select and download games
 
--- flash_cards.py
+++ flash_cards.py
@@ -3,11 +3,109 @@ import json
 import math
 import chess, chess.engine, chess.pgn
 from io import StringIO
-
 import numpy as np
 from PIL import Image
 import pyvips
 import csv
+import argparse
+import uuid
+
+
+def main(player_name, goof_threshold, stockfish_path, card_images_path):
+
+    game = get_latest_chess_com_game(player_name)
+
+    run_id = str(uuid.uuid4())
+
+    # did we play white or black
+    pov = get_pov(game, player_name)
+
+    board = game.board()
+    moves = game.mainline_moves()
+
+    engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
+
+    # create a list to keep track of our goofs
+    goofs = []
+
+    # iterate thru each move and eval the position after each one
+    for i, move in enumerate(moves):
+        
+        # skip this move if it isn't ours
+        if board.turn != pov:
+            board.push(move)
+            continue
+
+        # get a copy of the board before any moves are played 
+        before_board = board.copy()
+        
+        # what we played
+        player_move = board.san(move)
+
+        before_move_info = engine.analyse(board, limit=chess.engine.Limit(time=.1))
+        engine_move = before_move_info['pv'][0] if 'pv' in before_move_info else None
+        engine_move_san = board.san(engine_move)
+
+        before_move_eval = before_move_info["score"].pov(pov).score(mate_score=100000)
+        
+        board.push(move)
+
+        after_move_info = engine.analyse(board, limit=chess.engine.Limit(time=.1))
+        after_move_eval = after_move_info["score"].pov(pov).score(mate_score=100000)
+
+        # was this move a goof
+        if after_move_eval < before_move_eval and abs(before_move_eval - after_move_eval) >= goof_threshold:
+            
+            print(f'Went {player_move} but should have gone {engine_move_san}\n')
+            
+            # create a dict to house goof metadata and svgs
+            goof = {}
+            goof['move_num'] = math.ceil((i + 1) / 2)
+            goof['goof_factor'] = abs(before_move_eval - after_move_eval)
+            goof['engine_move'] = engine_move 
+    
+            arrow_player_move = chess.svg.Arrow(move.from_square, move.to_square, color='red')
+            goof['svg'] = chess.svg.board(before_board,
+                                        arrows=[arrow_player_move], 
+                                        orientation=pov, 
+                                        size=500)
+        
+            goof['svg_with_engine_move'] = chess.svg.board(before_board,
+                                                        arrows=[arrow_player_move, 
+                                                                chess.svg.Arrow(engine_move.from_square, engine_move.to_square)], 
+                                                        orientation=pov, 
+                                                        size=500)
+            goofs.append(goof)
+
+    engine.quit()
+
+    print(f'{len(goofs)} goof(s) this game')
+
+    # lets ease into this by sorting best to worst
+    sorted_goofs = sorted(goofs, key=lambda d: d['goof_factor']) 
+
+    img_names = []
+
+    for goof in sorted_goofs:
+        
+        front_img = svg_to_image(goof['svg'])
+        front_img_name = f"{run_id}_{goof['move_num']}_front.jpg"
+        front_img.save(f"{card_images_path}/{front_img_name}")
+
+        front_img_csv_path = f"<img src='{front_img_name}'/>"
+
+        back_img = svg_to_image(goof['svg_with_engine_move'])
+        back_img_name = f"{run_id}_{goof['move_num']}_back.jpg"
+        back_img.save(f"{card_images_path}/{back_img_name}")
+
+        back_img_csv_path = f"<img src='{back_img_name}'/>"
+
+        img_names.append((front_img_csv_path, back_img_csv_path))
+        
+
+    with open(f'{run_id}_flash_cards.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(img_names)
 
 
 def get_game_archives(player_name):
@@ -28,10 +126,24 @@ def get_latest_game_archive(game_archives):
     return get_game_archive(game_archives['archives'][-1])
 
 
+def get_latest_chess_com_game(player_name):
+
+    # get all the game archives
+    game_archives = get_game_archives(player_name)
+
+    # get the latest games archive
+    game_archive = get_latest_game_archive(game_archives)
+    print(f"{len(game_archive['games'])} games from {game_archives['archives'][-1]}")
+
+    # get a game
+    game_info = game_archive['games'][-1]
+
+    return chess.pgn.read_game(StringIO(game_info['pgn']))
+
 def get_pov(game, player_name):
 
     # are we playing white or black
-    if game['white']['username'] == player_name:
+    if game.headers['White']== player_name:
         return chess.WHITE
     return chess.BLACK
 
@@ -47,105 +159,16 @@ def svg_to_image(svg):
     return im.convert('RGB')
 
 
-player_name =  'bobhaffner'
+if __name__ == '__main__':
 
-game_archives = get_game_archives(player_name)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--player_name', dest='player_name', type=str, default='bobhaffner')
+    parser.add_argument('--goof_threshold', dest='goof_threshold', type=int, default=150, 
+                        help='Any move that drop the eval by more than the goof_threshold will be put in the flash card pile')
+    parser.add_argument('--stockfish_path', dest='stockfish_path', type=str, 
+                        default='/opt/homebrew/Cellar/stockfish/15/bin/stockfish')
+    parser.add_argument('--card_images_path', dest='card_images_path', type=str, 
+                        default='/Users/bob/Library/Application Support/Anki2/User 1/collection.media')
 
-game_archive = get_latest_game_archive(game_archives)
-print(f"{len(game_archive['games'])} games from {game_archives['archives'][-1]}")
-
-game_info = game_archive['games'][-6]
-
-pov = get_pov(game_info, player_name)
-
-# any move that drop the eval by more than the goof_threshold will be put in the flash card pile
-# lower goof_threshold = more flash cards
-# higher goof_threshold = less flash cards
-goof_threshold = 150
-
-stockfish_path = '/opt/homebrew/Cellar/stockfish/15/bin/stockfish'
-anki_media_path = '/Users/bob/Library/Application Support/Anki2/User 1/collection.media'
-
-# create some python-chess objs    
-# https://github.com/niklasf/python-chess
-engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
-game = chess.pgn.read_game(StringIO(game_info['pgn']))
-board = game.board()
-moves = game.mainline_moves()
-
-# create a list to keep track of our goofs
-goofs = []
-
-# iterate thru each move and eval the position after each one
-for i, move in enumerate(moves):
-    
-    if board.turn != pov:
-        board.push(move)
-        continue
-
-    # what was played
-    player_move = board.san(move)
-
-    before_move_info = engine.analyse(board, limit=chess.engine.Limit(time=.1))
-    engine_move = before_move_info['pv'][0] if 'pv' in before_move_info else None
-    engine_move_san = board.san(engine_move)
-
-    before_move_eval = before_move_info["score"].pov(pov).score(mate_score=100000)
-    
-    board.push(move)
-
-    after_move_info = engine.analyse(board, limit=chess.engine.Limit(time=.1))
-    after_move_eval = after_move_info["score"].pov(pov).score(mate_score=100000)
-
-    # was this move a goof
-    if after_move_eval < before_move_eval and abs(before_move_eval - after_move_eval) >= goof_threshold:
-        
-        print(f'Went {player_move} but should have gone {engine_move_san}\n')
-        
-        # create a dict to house goof metadata and svgs
-        goof = {}
-        goof['move_num'] = math.ceil((i + 1) / 2)
-        goof['goof_factor'] = abs(before_move_eval - after_move_eval)
-        goof['engine_move'] = engine_move 
-        
-        arrow_player_move = chess.svg.Arrow(move.from_square, move.to_square, color='red')
-        goof['svg'] = chess.svg.board(board,
-                                      arrows=[arrow_player_move], 
-                                      orientation=pov, 
-                                      size=500)
-        goof['svg_with_engine_move'] = chess.svg.board(board,
-                                                       arrows=[arrow_player_move, 
-                                                               chess.svg.Arrow(engine_move.from_square, engine_move.to_square)], 
-                                                       orientation=pov, 
-                                                       size=500)
-        goofs.append(goof)
-
-engine.quit()
-
-print(f'{len(goofs)} goof(s) this game')
-
-# lets ease into this by sorting best to worst
-sorted_goofs = sorted(goofs, key=lambda d: d['goof_factor']) 
-
-img_names = []
-
-for goof in sorted_goofs:
-    
-    front_img = svg_to_image(goof['svg'])
-    front_img_name = f"{game_info['uuid']}_{goof['move_num']}_front.jpg"
-    front_img.save(f"{anki_media_path}/{front_img_name}")
-
-    front_img_csv_path = f"<img src='{front_img_name}'/>"
-
-    back_img = svg_to_image(goof['svg_with_engine_move'])
-    back_img_name = f"{game_info['uuid']}_{goof['move_num']}_back.jpg"
-    back_img.save(f"{anki_media_path}/{back_img_name}")
-
-    back_img_csv_path = f"<img src='{back_img_name}'/>"
-
-    img_names.append((front_img_csv_path, back_img_csv_path))
-    
-
-with open('flash_cards.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerows(img_names)
+    args = parser.parse_args()
+    main(args.player_name, args.goof_threshold, args.stockfish_path, args.card_images_path)
